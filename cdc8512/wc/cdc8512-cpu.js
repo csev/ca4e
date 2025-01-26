cd4c2d65cdf3956ac9b6928451b95adc7379786c
Charles Severance <csev@umich.edu>
2025-01-26 09:36:56 -0500

    Make a gear icon sping and not spin

--- cdc8512/wc/cdc8512-cpu.js
+++ cdc8512/wc/cdc8512-cpu.js
@@ -1,25 +1,35 @@
 import {html, css, LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2.4.0/core/lit-core.min.js';
+import { msg } from 'https://cdn.jsdelivr.net/npm/@lit/localize@0.12.2/lit-localize.min.js';
 
 // https://stackoverflow.com/questions/68614776/using-lit-with-javascript-and-no-build-tools
 
 export class CDC8512CPU extends LitElement {
-  static get styles() {
-    return css`p { color: blue }`;
-  }
 
   static get properties() {
     return {
-      name: {type: String}
+      running: { type: Boolean }  
     }
   }
 
   constructor() {
     super();
-    this.name = 'Somebody';
+    this.running = false; 
   }
 
+  clickMe() {
+    this.running = !this.running; 
+  }
+
+  createRenderRoot() { return this;}
+
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+        <i class="fas fa-cog ${this.running ? "fa-spin" : "" }"></i>
+        <button class="btn btn-primary"
+            @click=${this.clickMe}>
+          ${this.running ? msg("Stop") : msg("Start")}
+        </button>
+    `;
   }
 }
 
