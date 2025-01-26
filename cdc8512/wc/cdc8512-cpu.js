c66964fc7337091e463c76da534547fa7686fd51
Charles Severance <csev@umich.edu>
2025-01-26 10:19:01 -0500

    Show memory and instructions

--- cdc8512/wc/cdc8512-cpu.js
+++ cdc8512/wc/cdc8512-cpu.js
@@ -1,5 +1,6 @@
 import {html, css, LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2.4.0/core/lit-core.min.js';
 import { msg } from 'https://cdn.jsdelivr.net/npm/@lit/localize@0.12.2/lit-localize.min.js';
+import { repeat } from 'https://unpkg.com/lit-html@2.7.4/directives/repeat.js';
 
 // https://stackoverflow.com/questions/68614776/using-lit-with-javascript-and-no-build-tools
 
@@ -7,17 +8,41 @@ export class CDC8512CPU extends LitElement {
 
   static get properties() {
     return {
-      running: { type: Boolean }  
+      running: { type: Boolean },
+      memory: { type: Array },
+      instructions: { type: Array }  
     }
   }
 
   constructor() {
     super();
-    this.running = false; 
+    this.running = false;
+    this.memory = [];
+    this.instructions = [];
+    for (let i = 0; i < 256; i++) {
+      this.memory.push(0);
+    }
+    for (let i = 0; i < 256; i++) {
+      this.instructions.push(0);
+    }
   }
 
-  clickMe() {
+  toggleRunning() {
     this.running = !this.running; 
+  }
+
+  changeInstruction(index) {
+    return (e) => {
+      console.log("changeInstruction", index, e.target.value);
+      this.instructions[index] = e.target.value;
+    }
+  }
+
+  changeMemory (index) {
+    return (e) => {
+      console.log("changeMemory", index, e.target.value);
+      this.memory[index] = e.target.value;
+    }
   }
 
   createRenderRoot() { return this;}
@@ -26,9 +51,27 @@ export class CDC8512CPU extends LitElement {
     return html`
         <i class="fas fa-cog ${this.running ? "fa-spin" : "" }"></i>
         <button class="btn btn-primary"
-            @click=${this.clickMe}>
+            @click=${this.toggleRunning}>
           ${this.running ? msg("Stop") : msg("Start")}
         </button>
+        <b>Memory</b>
+        <ul>
+          ${repeat(
+            this.memory,
+            (word, index) => html`
+            <li>${index}: <input type="text" value="${word}" @input=${this.changeMemory(index)}></li>
+            `
+          )}
+        </ul>
+        <b>Instructions</b>
+        <ul>
+          ${repeat(
+            this.instructions,
+            (word, index) => html`
+            <li>${index}: <input type="text" value="${word}" @input=${this.changeInstruction(index)}></li>
+            `
+          )}
+        </ul>
     `;
   }
 }
