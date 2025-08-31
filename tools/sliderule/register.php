e827dad158aad52a87b0f68ab3abc842f2422792
Charles Severance <csev@umich.edu>
2025-08-31 08:40:23 -0400

    First of many register.php

---
+++ tools/sliderule/register.php
@@ -0,0 +1,10 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "Slide Rule",
+"FontAwesome" => "fa-calculator",
+"short_name" => "Online Slide Rule",
+"description" => "This is an online tool for experimentation and supports auto graded for the assignments from www.ca4e.com (Computer Architecture for Everybody).",
+"messages" => array("launch", "launch_grade")
+);
+
