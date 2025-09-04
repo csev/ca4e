fc2444630b5d383adf83e68db2bed740f0932250
Charles Severance <csev@umich.edu>
2025-09-04 07:26:26 -0400

    Ask to be in a new window.

--- tools/mistic/register.php
+++ tools/mistic/register.php
@@ -7,5 +7,6 @@ $REGISTER_LTI2 = array(
 "description" => "This tool allows uers to create simple VLSI layouts by 'painting' metal and silicon traces to create digital cicuits suitable for manufacture by photo lithography.  Thsi tool also simulates the circuits after they are built.  The tool s a simplified version of the Magic VLSI layout tool.",
 "messages" => array("anonymous", "launch", "launch_grade"),
 "documentation" => "documentation.html",
+"targets" =>  array("window"),
 );
 
