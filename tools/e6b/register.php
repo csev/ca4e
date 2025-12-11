0c03a1643fcb6b5e5d277f6516c18f6887f4d48a
Charles Severance <csev@umich.edu>
2025-12-10 23:29:21 -0500

    Updates

--- tools/e6b/register.php
+++ tools/e6b/register.php
@@ -4,7 +4,7 @@ $REGISTER_LTI2 = array(
 "name" => "E6B Wind Calculator",
 "FontAwesome" => "fa-plane",
 "short_name" => "E6B",
-"description" => "This is an emulator for the E6B analog trigonometry computer that pilots use to compute windo correction angle for a particular wind speed, airspeed, and desired ground track.  It was developed in the late 1930s and is still a big part of private pilot training when preparting for cross country flights.",
+"description" => "This is an emulator for the E6B analog trigonometry computer that pilots use to compute wind correction angle for a particular wind speed, airspeed, and desired ground track.  It was developed in the late 1930s and is still a big part of private pilot training when preparting for cross country flights.",
 "messages" => array("anonymous", "launch", "launch_grade")
 );
 
