ce6867835a76ac05abc8f087931d11ef293564ef
Charles Severance <csev@umich.edu>
2026-01-25 16:13:58 -0500

    Switch where we get the web component

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 $foot = '
 <p style="font-size: 75%; margin-top: 5em;">
 Copyright Creative Commons Attribution 3.0 - Charles R. Severance
-</p><script type="module" src="' . htmlspecialchars($CFG->wwwroot.'/lms/announce/tsugi-announce.js') .'"></script>
+</p><script type="module" src="' . htmlspecialchars(\Tsugi\Controllers\StaticFiles::url('Announcements', 'tsugi-announce.js')) .'"></script>
 <script>
 $(document).ready( function () {
     $("#carousel").carousel();
